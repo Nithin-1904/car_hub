a1e168d0cceafad546a33685bd9505eb58bcdf4b
Nithin-1904 <nithinsarode7@gmail.com>
2024-03-18 16:44:03 +0530

    updated Code

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,12 @@
+import { CustomFilterProps } from '@/types'
+import React from 'react'
+
+const CustomFilter = ({title}:CustomFilterProps) => {
+  return (
+    <div title={title}>
+      CustomFIlter
+    </div>
+  )
+}
+
+export default CustomFilter
